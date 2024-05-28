270cd422dc22260a7731099c21bcdfc25e10325d
David Métivier <46794064+dmetivie@users.noreply.github.com>
2024-05-28 22:41:16 +0200

    fix type inference of vector of Distributions

--- ext/HiddenMarkovModelsDistributionsExt.jl
+++ ext/HiddenMarkovModelsDistributionsExt.jl
@@ -12,7 +12,7 @@ using Distributions:
 function HiddenMarkovModels.fit_in_sequence!(
     dists::AbstractVector{D}, i::Integer, x_nums::AbstractVector, w::AbstractVector
 ) where {D<:UnivariateDistribution}
-    return dists[i] = fit(D, x_nums, w)
+    return dists[i] = fit(typeof(dists[i]), x_nums, w)
 end
 
 function HiddenMarkovModels.fit_in_sequence!(
@@ -21,7 +21,7 @@ function HiddenMarkovModels.fit_in_sequence!(
     x_vecs::AbstractVector{<:AbstractVector},
     w::AbstractVector,
 ) where {D<:MultivariateDistribution}
-    return dists[i] = fit(D, reduce(hcat, x_vecs), w)
+    return dists[i] = fit(typeof(dists[i]), reduce(hcat, x_vecs), w)
 end
 
 function HiddenMarkovModels.fit_in_sequence!(
@@ -30,7 +30,7 @@ function HiddenMarkovModels.fit_in_sequence!(
     x_mats::AbstractVector{<:AbstractMatrix},
     w::AbstractVector,
 ) where {D<:MatrixDistribution}
-    return dists[i] = fit(D, reduce(dcat, x_mats), w)
+    return dists[i] = fit(typeof(dists[i]), reduce(dcat, x_mats), w)
 end
 
 dcat(M1, M2) = cat(M1, M2; dims=3)
